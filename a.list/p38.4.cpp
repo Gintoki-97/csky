08eb60b7d596a40392d634ca3b87f726ba7d0f2e
Gintoki-97 <gintoki-97@hotmail.com>
2019-04-28 23:03:01 +0800

    Tomorrow's kill time

---
+++ a.list/p38.4.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Gintoki on 2019/4/28.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "./a.linklist.cpp"
+
+/**
+ * Question:
+ * - 编写算法在带头结点的单链表中，删除一个最小值结点的高效算法（假设最小值结点唯一）；
+ **/
+ // TODO:
---
+++ a.list/p38.5.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Gintoki on 2019/4/28.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "./a.linklist.cpp"
+
+/**
+ * Question:
+ * - 编写算法将带头结点的单链表就地逆置，辅助空间复杂度要求为O(1)；
+ **/
+ // TODO:
