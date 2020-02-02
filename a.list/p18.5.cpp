8373ff76e97aa0265d1ce846560c310d0a57f87e
Gintoki-97 <gintoki-97@hotmail.com>
2020-02-02 18:41:30 +0800

    Distinct

--- a.list/p18.5.cpp
+++ a.list/p18.5.cpp
@@ -11,3 +11,34 @@
  * Question:
  * - 从有序顺序表中删除所有值重复的元素，使表中所有元素的值均不相同；
  **/
+void distinctList(SeqList* list) {
+
+    if (isEmpty(list)) {
+        printf("The sequence list is empty.");
+        return;
+    }
+
+    int i, j;
+
+    for (i = 0, j = 1; j < list->length; i++) {
+
+        while (list->data[j] == list->data[i]) {
+            j++;
+        }
+
+        if (j - i > 1) {
+            list->data[i + 1] = list->data[j];
+        }
+
+        j++;
+    }
+
+    list->length = i;
+}
+
+void testDistinctList() {
+    SeqList* list = generateSeqList(20);
+    printList(list);
+    distinctList(list);
+    printList(list);
+}
